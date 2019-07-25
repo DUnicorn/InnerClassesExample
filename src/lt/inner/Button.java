96d1e72c8b08bce394103c5cef7760509ebf7a40
DUnicorn <grigalionyte.donna@gmail.com>
2019-07-25 19:28:59 +0300

    second

--- src/lt/inner/Button.java
+++ src/lt/inner/Button.java
@@ -5,9 +5,7 @@ public class Button {
 	private String title;
 	private OnClickListener onClickLister;
 	
-	public Button(String title) {
-		this.title = title;
-	}
+	pakeitimai second second
 
 	public String getTitle() {
 		return title;
